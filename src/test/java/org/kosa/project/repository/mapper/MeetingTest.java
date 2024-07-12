891f99331f5fad7a4e6165231adaa23231ce5e2f
ChoiYH96 <169019916+ChoiYH96@users.noreply.github.com>
2024-07-12 21:28:55 +0900

    확인
    확인

--- src/test/java/org/kosa/project/repository/mapper/MeetingTest.java
+++ src/test/java/org/kosa/project/repository/mapper/MeetingTest.java
@@ -3,8 +3,8 @@ package org.kosa.project.repository.mapper;
 import org.junit.jupiter.api.Test;
 import org.kosa.project.controller.MeetingRegisterRequest;
 import org.kosa.project.service.MeetingService;
-import org.kosa.project.service.dto.MeetingRegisterDto;
-import org.kosa.project.service.dto.UserMeetingDto;
+import org.kosa.project.service.dto.meeting.MeetingRegisterDto;
+import org.kosa.project.service.dto.user.UserMeetingDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
