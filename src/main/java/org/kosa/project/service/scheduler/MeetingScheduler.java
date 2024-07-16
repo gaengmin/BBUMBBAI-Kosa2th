651374b79823c0099c458358779772b22702a82e
gaengmin <momo5736@naver.com>
2024-07-16 21:58:15 +0900

    상태 스케쥴링 및 리스트 상태변화
    상태 스케쥴링 및 리스트 상태변화

---
+++ src/main/java/org/kosa/project/service/scheduler/MeetingScheduler.java
@@ -0,0 +1,24 @@
+package org.kosa.project.service.scheduler;
+
+import org.kosa.project.service.MeetingService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MeetingScheduler {
+
+
+    private final MeetingService meetingService;
+
+    public MeetingScheduler(MeetingService meetingService) {
+        this.meetingService = meetingService;
+    }
+
+    //    @Scheduled(cron = "0 0 * * * *") //매시간마다 실행
+    @Scheduled(cron = "0 * * * * *") // 매 분마다 실행
+
+    public void updateMeetingEndTime() {
+        System.out.println("실행");
+        meetingService.updateMeetingEndTime();
+    }
+}
